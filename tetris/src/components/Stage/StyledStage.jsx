e2f50709ec9b6e8cbcfa0b557cb1b7c6aa432adb
Anibal Cañete <anibal_gustavo2010@hotmail.com>
2026-01-07 00:03:11 -0300

    Updated StyledStage.jsx and index.css Files

--- tetris/src/components/Stage/StyledStage.jsx
+++ tetris/src/components/Stage/StyledStage.jsx
@@ -3,24 +3,23 @@ import styled from "styled-components";
 
 export const StyledStage = styled.div`
     display: grid;
-    /*grid-template-rows: repeat(${props => props.height}, calc(25vw / ${props => props.width}));*/
     grid-template-columns: repeat(${props => props.width}, 1fr);
     grid-gap: 1px;
     border: 2px solid #333;
     width: 100%;
-    max-width: 25vw;
     background: #111;
+    max-width: 25vw;
     grid-template-rows: repeat(${props => props.height}, calc(25vw / ${props => props.width}));
 
-    
+
     @media (max-width: 600px) {
         max-width: 90vw;
         grid-template-rows: repeat(${props => props.height}, calc(90vw / ${props => props.width}));
     }
     
     @media (max-height: 600px) {
-        max-height: 60vh;
-        overflow-y: auto;
+        max-width: 90vw;
+        grid-template-rows: repeat(${props => props.height}, calc(90vw / ${props => props.width}));
     }
 
 `;
